e2b1d558e81d3f976c56ba8ddd74cd600982e2c6
Kirsty Pollock <k.pollock@reply.de>
2024-06-03 17:45:23 +0200

    ingress

--- scripts/09-ingress.sh
+++ scripts/09-ingress.sh
@@ -1,12 +1,17 @@
-COMMANDS="create-static-ip, delete, show"
+COMMANDS="create, delete, show"
 
 if [[ -z $1 ]]; then
     echo "must supply command [ $COMMANDS ] !"
     exit 1
 fi
 
+if [[ -z $2 ]]; then
+    IP_NAME=myAKSPublicIPForIngress
+else
+    IP_NAME=$2
+fi
+
 COMMAND=$1
-echo $COMMAND
 
 source $(dirname "$0")/set-credentials.sh
 source $(dirname "$0")/get-k8s-service-url.sh
@@ -15,19 +20,32 @@ function jsonArrayToTable() {
     jq -r '(.[0] | ([keys[] | .] |(., map(length*"-")))), (.[] | ([keys[] as $k | .[$k]])) | @tsv' | column -t -s $'\t'
 }
 
+MC_RG=$(az aks show --resource-group $RESOURCE_GROUP --name $AKS_CLUSTER_NAME --query nodeResourceGroup -o tsv)
+
 case $COMMAND in
-"create-static-ip")
-    RG=$(az aks show --resource-group $RESOURCE_GROUP --name $AKS_CLUSTER_NAME --query nodeResourceGroup -o tsv)
-    IP_ADDRESS=$(az network public-ip create --resource-group $RG --name myAKSPublicIPForIngress --sku Standard --allocation-method static --query publicIp.ipAddress -o tsv)
-    
-    az network public-ip list | jq .[] | map()
+"create")
+    IP_ADDRESS=$(az network public-ip create --resource-group $MC_RG --name $IP_NAME --sku Standard --allocation-method static --query publicIp.ipAddress -o tsv)
+
     echo
     echo IP ADDRESS
-    echo ==========
+    echo =============
     echo $IP_ADDRESS
     ;;
-"delete") ;;
-"show") ;;
+"delete")
+    az network public-ip delete --resource-group $MC_RG --name $IP_NAME
+    az network public-ip show --resource-group $MC_RG --name $IP_NAME
+    ;;
+"show")
+    IP_ADDRESS=$(az network public-ip list | jq -r --arg NAME "$IP_NAME" '.[] | select(.name == $NAME) | .ipAddress')
+    echo IP ADDRESS
+    echo =============
+    echo $IP_ADDRESS
+    echo
+    echo DETAILS
+    echo =============
+    az network public-ip show --resource-group $MC_RG --name $IP_NAME
+
+    ;;
 \?)
     echo "Invalid command. Use one of [ $COMMANDS ] " >&2
     exit 1
